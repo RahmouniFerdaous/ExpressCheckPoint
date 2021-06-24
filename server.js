ef7f44b664fc476ca8957343c2f3a94676a3e192
Rahmouni <ferdaous.rahmouni@gmail.com>
2021-06-24 12:49:12 +0100

    secondCommit

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 //Node js Test 
+const path=require('path')
 console.log('my first app express with node js')
 //import express 
 const express=require('express')
-// app is an express instance : associate express methods to a variable
+// app is an express () instance : associate express methods to a variable
 const app=express();
+//todat is a an Date() instance 
+const today=new Date(Date.now())
 // my global middleware 
-const day= new Date().getDay();
-const hours=new Date().getHours();
+const day= today.getDay();
+const hours=today.getHours();
 const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+app.get('/error.html',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','error.html')) 
+})
 app.use((req,res,next)=>{
     console.log("A new request received at "+ days[day] +" "+ hours +" H" );
-(day === 0 || day === 6) && ( 9<= hours <= 17) ? // day[0-6] // hours[0-23]
-res.status(404).send('<h1>The web application is only available during working hours (Monday to Friday,  from 9 to 17)</h1>')
+(day === 0 || day === 6) || (hours <9 || hours >= 17)  ? // day[0-6] // hours[0-23]
+// res.status(404).send('<h1>Only available during working hours (Monday to Friday,  from 9 to 17)</h1>')
+res.redirect('/error.html')  // redirect to an html file with new path 
 :next();   // to the next middelware! 
 })
 //rendering static html files ( public: folder that contains our html files and take the index.html as first rendering page )
